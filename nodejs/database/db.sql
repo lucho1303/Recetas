730322ed890d8b1e056e01557d5e870fbe317d49
lucho1303 <benavides.lucho@hotmail.com>
2020-10-22 01:25:37 -0500

    Actualzacion de bdd

--- nodejs/database/db.sql
+++ nodejs/database/db.sql
@@ -1,25 +1,46 @@
+
+
+
 CREATE TABLE usuarios(
-    id INT(11) NOT NULL,
-    username VARCHAR(16) NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    fullname VARCHAR(100) NOT NULL
+	id INT(11) NOT NULL,
+	nombre VARCHAR(30) NOT NULL,
+	apellido VARCHAR(30) NOT NULL,
+	username VARCHAR(16) NOT NULL,
+	correo VARCHAR(255) NOT NULL,
+	clave VARCHAR(60) NOT NULL
 );
+
 ALTER TABLE usuarios
-    ADD PRIMARY KEY(id);
+	ADD PRIMARY KEY (id);
 ALTER TABLE usuarios
-    MODIFY id INT(11) NOT NULL AUTO_INCREMENT;
+	MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
 
-
-CREATE TABLE links(
-    id INT(11) NOT NULL,
-    titulo VARCHAR(30)NOT NULL,
-    descritption TEXT,
-    user_id INT(11),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES usuarios(id)
+CREATE TABLE recetas(
+	id INT(11) NOT NULL,
+	title VARCHAR(150) NOT NULL,
+	description TEXT,
+	ingredientes TEXT,
+	user_id INT(11),
+	image VARCHAR(100) NOT NULL,
+	estado bool,
+	created_at timestamp NOT NULL DEFAULT current_timestamp,
+	CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES usuarios(id)
 );
-ALTER TABLE links
-    ADD PRIMARY KEY(id);
-ALTER TABLE links
-    MODIFY id INT(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE recetas
+	ADD PRIMARY KEY (id);
+ALTER TABLE recetas
+	MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+CREATE TABLE ingredientes(
+	id INT(11) NOT NULL,
+	name VARCHAR(100) NOT NULL,
+	user_id INT(11),
+	estado bool,
+	CONSTRAINT fk_in_user FOREIGN KEY(user_id) REFERENCES usuarios(id)
+);
+ALTER TABLE ingredientes
+	ADD PRIMARY KEY (id);
+ALTER TABLE ingredientes
+	MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
