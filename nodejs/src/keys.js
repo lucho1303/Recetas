b9123c46aa1bd1e68d0ca155db22f58469f58455
lucho1303 <benavides.lucho@hotmail.com>
2020-10-22 23:19:23 -0500

    Configuracion de la base de datos online

--- nodejs/src/keys.js
+++ nodejs/src/keys.js
@@ -1,11 +1,19 @@
 module.exports = {
-    
+    //local
+    /*
     database: {
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'database_recetas'
-        //database: 'dev_recetas'
+        //database: 'database_recetas'
+        database: 'dev_recetas'
     }
-
+    */
+    //online db4free
+    database: {
+        host: 'db4free.net',
+        user: 'dev_recetas_us',
+        password: 'Temporal',
+        database: 'dev_recetas_ol'
+    }
 };
